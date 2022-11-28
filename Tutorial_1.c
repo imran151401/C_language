539fa42c409c1b0a4442e15cd0928004c29b8e98
ONi Islam <96674607+imran151401@users.noreply.github.com>
2022-11-28 10:27:02 +0600

    Tutorial_1.c
    Even odd

---
+++ Tutorial_1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+
+    int i, a;
+
+    printf("Enter the last value:");
+
+    scanf("%d", &a);
+
+    for(i=0; i<=a; i=i+2){
+
+        printf("%d\n", i); 
+
+        
+
+    }
+
+    
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+    
+
+
+
+
+
